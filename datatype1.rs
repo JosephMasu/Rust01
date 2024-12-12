df508102596e9f79d5268abf4ea2648b0a8d4224
josephMasu <josephmuhindo089@gmail.com>
2024-12-12 13:10:09 +0200

    the integers

---
+++ datatype1.rs
@@ -0,0 +1,19 @@
+//primirive data types are int, float, bool, char
+
+fn main() {
+    let x = 5; //integer
+    let y = 2.5; //float
+    let z = true; //boolean
+    let a = 'a'; //character
+
+
+    let d: i8 = -42;
+    let c: u64 = 100;
+
+    println!("x is: {}", x);
+    println!("y is: {}", y);
+    println!("z is: {}", z);
+    println!("a is: {}", a);
+    println!("Unsigned Integer is: {}", d);
+    println!("Signed Integer is: {}", c);
+}
