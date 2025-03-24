c6515272b5b657aba69e4d4664ff838c829aad43
josephMasu <josephmuhindo089@gmail.com>
2025-03-24 23:09:21 +0300

    parameters and args in a function

---
+++ _function/function3.rs
@@ -0,0 +1,11 @@
+
+
+fn another_function(x: i32, letter: char) {
+    println!("The value of x is: {}", x);
+    println!("The value of letter is: {}", letter);
+}
+
+fn main() {
+    println!("Hello, world!");
+    another_function(5, 'M');
+}
