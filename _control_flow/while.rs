473f9bfc84c007e356700af6f27a187d5b3b57b4
josephMasu <josephmuhindo089@gmail.com>
2025-03-26 00:36:35 +0300

    for and while loop

---
+++ _control_flow/while.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut count = 0;
+    
+    // while count < 5 {
+    //     println!("Count: {}", count);
+    //     count += 1;  // Increment the counter
+    // }
+
+    for i in count..5 {
+        println!("Count: {}", i);
+        count += 1;  // Increment the counter
+    }
+
+}
